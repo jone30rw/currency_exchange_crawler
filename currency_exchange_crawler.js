c5e15cf209cef18554e2602ab57be81b2458b9b9
Mohammad Hassany <jone30rw@gmail.com>
2020-05-11 01:33:31 -0400

    updated to match dab new exch_rate page structure

--- currency_exchange_crawler.js
+++ currency_exchange_crawler.js
@@ -1,41 +1,33 @@
 /**
  * @author Mohammad Hassany <jone30rw@gmail.com>
  * 
- * The get_currency_rates function will download the specified url (http://dab.gov.af/en/dab/currency) then use the jQuery find function to find the tr's containing the exchange rates.
- * Note: This is snippet only works with current format of DAB (Da Afghanistan Bank) html structure, so it may not work in future.
+ * The load_dab_exchange_rates function will load the http://dab.gov.af/exchange-rates page, 
+ * then parse the content for the exchange rates.
  *
  * Example:
- *   // call the function with a callback function
- *   get_currency_rates(function(currency_rates) {
+ *   // call the function with a callback function,
+ *   load_dab_exchange_rates(function(exchange_rates) {
  *     // for now, just print it in console
- *     console.log(currency_rates);
- *   });
+ *     console.log(exchange_rates);
+ *   }, '01/01/2020');
  */
-function get_currency_rates(callback) {
+function load_dab_exchange_rates(callback, date='') {
     // use jQuery get to load the html page
-    $.get('http://dab.gov.af/en/dab/currency', function(html) {
-        var exchange_rates = [];
+    $.get('https://dab.gov.af/exchange-rates?field_date_value=' + date, function(html) {
+        const exchange_rates = [];
+        $('main .table-responsive').first().find('tbody > tr').each(function() {
+            const cols = $(this).find('td');
 
-        // find the tr's that contains the exchange rates
-        $(html).find('#dataContainer div.Contents > table:first > tbody > tr').each(function() {
-            var exchange_tr = $(this);
-
-            // discard the tr if it doesn't contain the currency name
-            if (exchange_tr.find('td:nth-child(1)').text().length < 1) 
-                return;
-
-            // find and store the specific values in json array
-            exchange_rates.push({
-                'currency': exchange_tr.find('td:nth-child(1)').text(),
-                'cash_sell': exchange_tr.find('td:nth-child(2)').text(),
-                'cash_buy': exchange_tr.find('td:nth-child(3)').text(),
-                'trans_sell': exchange_tr.find('td:nth-child(4)').text(),
-                'trans_buy': exchange_tr.find('td:nth-child(5)').text()
+            exchange_rates.push({ 
+                currency: cols[0].innerText, 
+                cash_sell: cols[1].innerText, 
+                cash_buy: cols[2].innerText, 
+                transfer_sell: cols[3].innerText, 
+                transfer_buy: cols[4].innerText
             });
         });
     
         // if there is callback, then call it with json values
-        if (callback)
-            callback(exchange_rates);
+        if (callback) callback(exchange_rates);
     });
 }
